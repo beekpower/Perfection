ef5ce2c96758e4754965ddbad92cf2afe2d4d0dd
Phillip Igoe <phillipigoe@gmail.com>
2015-10-14 11:10:08 -0400

    format perfection + util classes

--- src/perfection.cpp
+++ src/perfection.cpp
@@ -1,10 +1,10 @@
 #ifdef __linux__
-    #include "GL/freeglut.h"
-    #include "GL/gl.h"
+#include "GL/freeglut.h"
+#include "GL/gl.h"
 #elif _WIN32
-    // Put your windows imports here
+// Put your windows imports here
 #elif __APPLE__
-    #include <freeglut.h>
+#include <freeglut.h>
 #endif
 
 #include <stdio.h>
@@ -32,38 +32,38 @@ Piece *selectedPiece;
 Board *board;
 
 void draw() {
-  //Clear the whole screen
-  glClear(GL_COLOR_BUFFER_BIT);
+    //Clear the whole screen
+    glClear(GL_COLOR_BUFFER_BIT);
     // Draw board
     board->draw();
-
+    
     // Draw shapes
     int row = 0;
     if (selectedPiece != NULL) {
-      selectedPiece->setLoc(mouseX, mouseY);
+        selectedPiece->setLoc(mouseX, mouseY);
     }
-
+    
     //Loop through each shape
     for (int i = 0; i < 25; i++) {
-      pieces[i]->draw();
+        pieces[i]->draw();
     }
-
-  glFlush();
+    
+    glFlush();
 }
 
 void processNormalKeys(unsigned char key, int x, int y) {
-
-  if (selectedPiece != NULL) {
-    if (key == 'a') {
-      selectedPiece->rotate(1);
-      glutPostRedisplay();
+    
+    if (selectedPiece != NULL) {
+        if (key == 'a') {
+            selectedPiece->rotate(1);
+            glutPostRedisplay();
+        }
+        
+        if (key == 's') {
+            selectedPiece->rotate(-1);
+            glutPostRedisplay();
+        }
     }
-
-    if (key == 's') {
-      selectedPiece->rotate(-1);
-      glutPostRedisplay();
-    }
-  }
 }
 
 void init(void) {
@@ -76,10 +76,10 @@ void init(void) {
     //Initialy clear the screen
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
-
+    
     // Initialize the board object
     board = new Board(WINDOW_WIDTH, WINDOW_HEIGHT);
-
+    
     // Initialize all piece objects
     int row = 0;
     for (int i = 0; i < 25; i++) {
@@ -87,58 +87,58 @@ void init(void) {
         if(((i+1)%5) == 0) {
             row++;
         }
-
+        
     }
-
+    
 }
 
 /*Mouse event*/
 void mouse(int button, int state, int x, int y) {
-
-  x = x - (WINDOW_WIDTH / 2);
-  y = -(y - (WINDOW_HEIGHT / 2));
-
-  switch (button) {
-    case GLUT_LEFT_BUTTON:   //When something is left clicked
-      if(state == GLUT_UP) {
-        if (selectedPiece == NULL) {
-          for (int i = 0; i < 25; i++) {
-            if (pieces[i]->clicked(x, y))
-            {
-              selectedPiece = pieces[i];
-              break;
+    
+    x = x - (WINDOW_WIDTH / 2);
+    y = -(y - (WINDOW_HEIGHT / 2));
+    
+    switch (button) {
+        case GLUT_LEFT_BUTTON:   //When something is left clicked
+            if(state == GLUT_UP) {
+                if (selectedPiece == NULL) {
+                    for (int i = 0; i < 25; i++) {
+                        if (pieces[i]->clicked(x, y))
+                        {
+                            selectedPiece = pieces[i];
+                            break;
+                        }
+                    }
+                } else {
+                    selectedPiece->restoreInitialLocation();
+                    selectedPiece = NULL;
+                }
             }
-          }
-        } else {
-          selectedPiece->restoreInitialLocation();
-          selectedPiece = NULL;
-        }
-      }
-      break;
-  }
-
-  glutPostRedisplay();
+            break;
+    }
+    
+    glutPostRedisplay();
 }
 
 void mouseMove(int x, int y) {
-  mouseX = x - (WINDOW_WIDTH / 2);
-  mouseY = -(y - (WINDOW_HEIGHT / 2));
-  glutPostRedisplay();
+    mouseX = x - (WINDOW_WIDTH / 2);
+    mouseY = -(y - (WINDOW_HEIGHT / 2));
+    glutPostRedisplay();
 }
 
 /*Main entry point for the progam*/
 int main(int argc, char** argv) {
-  srand(time(NULL)); //setup random seed
-  glutInit(&argc, argv);                           //init glut
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);     //set display mode
-  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT); //set the window size
-  glutInitWindowPosition(100, 150);                 //set the window position
-  glutCreateWindow("Project 1 Shapes");                   //create the window and window name
-  glutKeyboardFunc(processNormalKeys);
-  glutMouseFunc(mouse);                             //set the mouse event function
-  glutPassiveMotionFunc(mouseMove);
-  glutDisplayFunc(draw);                     //set the redraw function                            //set the mouse event function
-  init();                                           //init function
-  glutMainLoop();                                   //enter the glut loop
-  return (0);
+    srand(time(NULL)); //setup random seed
+    glutInit(&argc, argv);                           //init glut
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);     //set display mode
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT); //set the window size
+    glutInitWindowPosition(100, 150);                 //set the window position
+    glutCreateWindow("Project 1 Shapes");                   //create the window and window name
+    glutKeyboardFunc(processNormalKeys);
+    glutMouseFunc(mouse);                             //set the mouse event function
+    glutPassiveMotionFunc(mouseMove);
+    glutDisplayFunc(draw);                     //set the redraw function                            //set the mouse event function
+    init();                                           //init function
+    glutMainLoop();                                   //enter the glut loop
+    return (0);
 }
--- src/util.cpp
+++ src/util.cpp
@@ -1,29 +1,16 @@
 #include "util.h"
 
 int Util::randomNumber(int min, int max) {
-  return min + (rand() % (int)(max - min + 1));
+    return min + (rand() % (int)(max - min + 1));
 }
 
 void Util::drawCircle(int radius, int startDegree, int endDegree, bool drawBorder) {
-  glBegin(GL_POLYGON);
-    if(drawBorder) {
-        glColor3ub(230, 221, 42);
-    } else {
-        glColor3ub(64, 101, 147);
-    }
-    for (int i=startDegree; i != endDegree; i++) {
-      float degInRad = i*0.0174532925;
-      glVertex2f(cos(degInRad)*radius,sin(degInRad)*radius);
-      if (i == 360) {
-        i = 0;
-      }
-    }
-  glEnd();
-
-
-    if(drawBorder) {
-        glBegin(GL_LINE_LOOP);
-        glColor3ub(0, 0, 0);
+    glBegin(GL_POLYGON);
+        if(drawBorder) {
+            glColor3ub(230, 221, 42);
+        } else {
+            glColor3ub(64, 101, 147);
+        }
         for (int i=startDegree; i != endDegree; i++) {
             float degInRad = i*0.0174532925;
             glVertex2f(cos(degInRad)*radius,sin(degInRad)*radius);
@@ -31,29 +18,30 @@ void Util::drawCircle(int radius, int startDegree, int endDegree, bool drawBorde
                 i = 0;
             }
         }
+    glEnd();
+    
+    
+    if(drawBorder) {
+        glBegin(GL_LINE_LOOP);
+            glColor3ub(0, 0, 0);
+            for (int i=startDegree; i != endDegree; i++) {
+                float degInRad = i*0.0174532925;
+                glVertex2f(cos(degInRad)*radius,sin(degInRad)*radius);
+                if (i == 360) {
+                    i = 0;
+                }
+            }
         glEnd();
     }
 }
 
 void Util::drawCircle(int radius, bool drawBorder) {
-  glBegin(GL_POLYGON);
-    if(drawBorder) {
-        glColor3ub(230, 221, 42);
-    } else {
-        glColor3ub(64, 101, 147);
-    }
-    for (int i=0; i < 360; i++) {
-      float degInRad = i*0.0174532925;
-      glVertex2f(cos(degInRad)*radius,sin(degInRad)*radius);
-      if (i == 360) {
-        i = 0;
-      }
-    }
-  glEnd();
-
-    if(drawBorder) {
-        glBegin(GL_LINE_LOOP);
-        glColor3ub(0, 0, 0);
+    glBegin(GL_POLYGON);
+        if(drawBorder) {
+            glColor3ub(230, 221, 42);
+        } else {
+            glColor3ub(64, 101, 147);
+        }
         for (int i=0; i < 360; i++) {
             float degInRad = i*0.0174532925;
             glVertex2f(cos(degInRad)*radius,sin(degInRad)*radius);
@@ -61,34 +49,46 @@ void Util::drawCircle(int radius, bool drawBorder) {
                 i = 0;
             }
         }
+    glEnd();
+    
+    if(drawBorder) {
+        glBegin(GL_LINE_LOOP);
+            glColor3ub(0, 0, 0);
+            for (int i=0; i < 360; i++) {
+                float degInRad = i*0.0174532925;
+                glVertex2f(cos(degInRad)*radius,sin(degInRad)*radius);
+                if (i == 360) {
+                    i = 0;
+                }
+            }
         glEnd();
     }
 }
 
 void Util::drawPolygon(int n, int radius, bool drawBorder) {
-  float angleIncrement = 360.0f / n;
-  angleIncrement *= M_PI / 180.0f;
-  glBegin(GL_TRIANGLE_FAN);
-    if(drawBorder) {
-        glColor3ub(230, 221, 42);
-    } else {
-        glColor3ub(64, 101, 147);
-    }
-    float angle = 0.0f;
-    for (int k = 0; k < n; ++k) {
-      glVertex2f(radius * cos(angle), radius * sin(angle));
-      angle += angleIncrement;
-    }
-  glEnd();
-
-    if(drawBorder) {
-        glBegin(GL_LINE_LOOP);
-        glColor3ub(0, 0, 0);
+    float angleIncrement = 360.0f / n;
+    angleIncrement *= M_PI / 180.0f;
+    glBegin(GL_TRIANGLE_FAN);
+        if(drawBorder) {
+            glColor3ub(230, 221, 42);
+        } else {
+            glColor3ub(64, 101, 147);
+        }
         float angle = 0.0f;
         for (int k = 0; k < n; ++k) {
             glVertex2f(radius * cos(angle), radius * sin(angle));
             angle += angleIncrement;
         }
+    glEnd();
+    
+    if(drawBorder) {
+        glBegin(GL_LINE_LOOP);
+            glColor3ub(0, 0, 0);
+            float angle = 0.0f;
+            for (int k = 0; k < n; ++k) {
+                glVertex2f(radius * cos(angle), radius * sin(angle));
+                angle += angleIncrement;
+            }
         glEnd();
     }
 }
